f8e51ba98a7cee998cf7af204be7b56e78f979e8
FrostLab81 <thiagodd334@gmail.com>
2025-08-24 19:03:15 -0300

    odessa: clone motocam automatically

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# clone motocam
+git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_whitney.git --depth=1 -b eleven-arm64 packages/apps/MotCamera2
+
+# clone overlay
+git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_overlay.git -b ten packages/apps/MotCamera2-overlay
+
+# clone signature
+git clone https://gitlab.com/NemesisDevelopers/motorola/motorola_motosignatureapp.git -b eleven packages/apps/MotoSignatureApp
